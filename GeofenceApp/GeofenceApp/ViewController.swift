511b870f0f3c11cf5db76aa0e2f744aeb6b74d4d
Andrew Popov <apopov@brightgrove.com>
2019-01-08 12:25:59 +0200

    Added an initial version of the project.

---
+++ GeofenceApp/GeofenceApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeofenceApp
+//
+//  Created by Andrii on 1/8/19.
+//  Copyright © 2019 Andrii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
